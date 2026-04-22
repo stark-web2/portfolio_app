2f9d513e905d05a3530724427ef9931bcf5c3906
Crystal Bangalisan <crystalbangalisan64@gmail.com>
2026-04-22 08:05:48 +0800

    Update contact page with Facebook link
    Added Facebook link to the contact page.

--- pages/contact.py
+++ pages/contact.py
@@ -6,7 +6,6 @@ st.title("📞 Contact Me")
 
 st.write("Feel free to send me a message 👇")
 
-# ===== FORM =====
 with st.form("contact_form"):
     name = st.text_input("Name")
     email = st.text_input("Email")
@@ -22,7 +21,6 @@ with st.form("contact_form"):
 
 st.divider()
 
-# ===== SOCIAL LINKS =====
 st.subheader("🔗 Social Links")
 st.write("GitHub: https://github.com/stark-web2")
 st.write("Facebook: https://www.facebook.com/crystal.bangalisan")
